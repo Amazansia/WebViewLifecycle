4a1092d3d96c28443612e4993ab6d163eb94e7f7
sia.a <sia.a@kakaocorp.com>
2024-04-02 11:22:01 +0900

    공유용 커밋

---
+++ app/src/main/java/com/example/webviewlifecycle/LoggedWebViewClient.kt
@@ -0,0 +1,154 @@
+package com.example.webviewlifecycle
+
+import android.graphics.Bitmap
+import android.net.http.SslError
+import android.os.Message
+import android.util.Log
+import android.view.KeyEvent
+import android.webkit.ClientCertRequest
+import android.webkit.HttpAuthHandler
+import android.webkit.RenderProcessGoneDetail
+import android.webkit.SafeBrowsingResponse
+import android.webkit.SslErrorHandler
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+private const val TAG = "LoggedWebViewClient"
+
+class LoggedWebViewClient() : WebViewClient() {
+    @Deprecated(
+        "Deprecated in Java",
+        ReplaceWith("super.shouldOverrideUrlLoading(view, url)", "android.webkit.WebViewClient")
+    )
+    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+        Log.d(TAG, "shouldOverrideUrlLoading")
+        return super.shouldOverrideUrlLoading(view, url)
+    }
+
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        Log.d(TAG, "shouldOverrideUrlLoading")
+        return super.shouldOverrideUrlLoading(view, request)
+    }
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        Log.d(TAG, "onPageStarted")
+        super.onPageStarted(view, url, favicon)
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        Log.d(TAG, "onPageFinished")
+        super.onPageFinished(view, url)
+    }
+
+    override fun onLoadResource(view: WebView?, url: String?) {
+        Log.d(TAG, "onLoadResource: $url")
+        super.onLoadResource(view, url)
+    }
+
+    override fun onPageCommitVisible(view: WebView?, url: String?) {
+        Log.d(TAG, "onPageCommitVisible")
+        super.onPageCommitVisible(view, url)
+    }
+
+    override fun shouldInterceptRequest(view: WebView?, url: String?): WebResourceResponse? {
+        Log.d(TAG, "shouldInterceptRequest: $url")
+        return super.shouldInterceptRequest(view, url)
+    }
+
+    override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse? {
+        Log.d(TAG, "shouldInterceptRequest")
+        return super.shouldInterceptRequest(view, request)
+    }
+
+    override fun onTooManyRedirects(view: WebView?, cancelMsg: Message?, continueMsg: Message?) {
+        Log.d(TAG, "onTooManyRedirects")
+        super.onTooManyRedirects(view, cancelMsg, continueMsg)
+    }
+
+    override fun onReceivedError(view: WebView?, errorCode: Int, description: String?, failingUrl: String?) {
+        Log.d(TAG, "onReceivedError")
+        super.onReceivedError(view, errorCode, description, failingUrl)
+    }
+
+    override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+        Log.d(TAG, "onReceivedError")
+        super.onReceivedError(view, request, error)
+    }
+
+    override fun onReceivedHttpError(
+        view: WebView?,
+        request: WebResourceRequest?,
+        errorResponse: WebResourceResponse?,
+    ) {
+        Log.d(TAG, "onReceivedHttpError")
+        super.onReceivedHttpError(view, request, errorResponse)
+    }
+
+    override fun onFormResubmission(view: WebView?, dontResend: Message?, resend: Message?) {
+        Log.d(TAG, "onFormResubmission")
+        super.onFormResubmission(view, dontResend, resend)
+    }
+
+    override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
+        Log.d(TAG, "doUpdateVisitedHistory")
+        super.doUpdateVisitedHistory(view, url, isReload)
+    }
+
+    override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+        Log.d(TAG, "onReceivedSslError")
+        super.onReceivedSslError(view, handler, error)
+    }
+
+    override fun onReceivedClientCertRequest(view: WebView?, request: ClientCertRequest?) {
+        Log.d(TAG, "onReceivedClientCertRequest")
+        super.onReceivedClientCertRequest(view, request)
+    }
+
+    override fun onReceivedHttpAuthRequest(
+        view: WebView?,
+        handler: HttpAuthHandler?,
+        host: String?,
+        realm: String?,
+    ) {
+        Log.d(TAG, "onReceivedHttpAuthRequest")
+        super.onReceivedHttpAuthRequest(view, handler, host, realm)
+    }
+
+    override fun shouldOverrideKeyEvent(view: WebView?, event: KeyEvent?): Boolean {
+        Log.d(TAG, "shouldOverrideKeyEvent")
+        return super.shouldOverrideKeyEvent(view, event)
+    }
+
+    override fun onUnhandledKeyEvent(view: WebView?, event: KeyEvent?) {
+        Log.d(TAG, "onUnhandledKeyEvent")
+        super.onUnhandledKeyEvent(view, event)
+    }
+
+    override fun onScaleChanged(view: WebView?, oldScale: Float, newScale: Float) {
+        Log.d(TAG, "onScaleChanged")
+        super.onScaleChanged(view, oldScale, newScale)
+    }
+
+    override fun onReceivedLoginRequest(view: WebView?, realm: String?, account: String?, args: String?) {
+        Log.d(TAG, "onReceivedLoginRequest")
+        super.onReceivedLoginRequest(view, realm, account, args)
+    }
+
+    override fun onRenderProcessGone(view: WebView?, detail: RenderProcessGoneDetail?): Boolean {
+        Log.d(TAG, "onRenderProcessGone")
+        return super.onRenderProcessGone(view, detail)
+    }
+
+    override fun onSafeBrowsingHit(
+        view: WebView?,
+        request: WebResourceRequest?,
+        threatType: Int,
+        callback: SafeBrowsingResponse?,
+    ) {
+        Log.d(TAG, "onSafeBrowsingHit")
+        super.onSafeBrowsingHit(view, request, threatType, callback)
+    }
+}
--- app/src/main/java/com/example/webviewlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/webviewlifecycle/MainActivity.kt
@@ -1,19 +1,45 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.webviewlifecycle
 
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.webviewlifecycle.ui.theme.WebviewLifecycleTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +48,12 @@ class MainActivity : ComponentActivity() {
             WebviewLifecycleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Scaffold(
+                        bottomBar = { WebviewBottomBar() },
+                        topBar = { WebviewTopBar() }
+                    ) {
+                        AndroidTestWebView(it)
+                    }
                 }
             }
         }
@@ -30,26 +61,144 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                webViewClient = WebViewClient()
-                settings.loadWithOverviewMode = true
-                settings.useWideViewPort = true
-                settings.setSupportZoom(true)
-            }
-        },
-        update = { webView ->
-            webView.loadUrl("https://www.daum.net/")
+fun WebviewBottomBar() {
+    BottomAppBar {
+        BottomBarBackButton()
+        BottomBarForwardButton()
+        BottomBarRefreshButton()
+        // 기타기능 버튼: 링크 복사, 다른 앱으로 공유, 다른 브라우저로 열기...(생략)
+    }
+}
+
+@Composable
+fun BottomBarRefreshButton() {
+//    TODO("Not yet implemented")
+}
+
+@Composable
+fun BottomBarForwardButton() {
+//    TODO("Not yet implemented")
+}
+
+@Composable
+fun BottomBarBackButton() {
+    IconButton(onClick = { }) {
+
+    }
+}
+
+@Composable
+fun WebviewTopBar() {
+    TopAppBar(
+        navigationIcon = { TopBarNaviIcon() },
+        title = { WebviewAddressBar() },
+        actions = {
+            // 카카오톡으로 공유
+            // 둥둥이로 변환
         }
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WebviewLifecycleTheme {
-        Greeting("Android")
+fun TopBarNaviIcon() {
+//    TODO("Not yet implemented")
+}
+
+@Composable
+fun WebviewAddressBar() {
+//    TODO("Not yet implemented")
+}
+
+@Composable
+fun AndroidTestWebView(paddingValues: PaddingValues) {
+    Column {
+        LinearDeterminateIndicator()
+        AndroidView(
+            factory = { context ->
+                WebView(context).apply {
+                    isFocusable = true
+                    isFocusableInTouchMode = true
+                    webViewClient = LoggedWebViewClient()
+                    webChromeClient = WebChromeClient()
+
+                    // settings
+                    settings.javaScriptEnabled = true
+                    settings.setSupportMultipleWindows(false)
+                    settings.javaScriptCanOpenWindowsAutomatically = true
+                    settings.domStorageEnabled = true
+                    settings.allowContentAccess = false
+                    settings.loadWithOverviewMode = true
+                    settings.useWideViewPort = true
+                    settings.builtInZoomControls = true
+                    settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING
+                    settings.setNeedInitialFocus(false)
+                    settings.databaseEnabled = true
+                    settings.setGeolocationEnabled(true)
+                    settings.safeBrowsingEnabled = false
+
+                    settings.cacheMode = WebSettings.LOAD_NO_CACHE
+                    settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+                    // webChromeClient not null
+//                webChromeClient?.onCloseWindow(this)
+//                webChromeClient?.
+                }
+            },
+            update = { webView ->
+                webView.loadUrl("https://www.daum.net/")
+                Log.d(TAG, webView.progress.toString())
+            }
+        )
+
     }
 }
+
+@Composable
+fun LinearDeterminateIndicator() {
+    var currentProgress: Float by remember { mutableStateOf(0f) }
+    var loading by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope() // Create a coroutine scope
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Button(onClick = {
+            loading = true
+            scope.launch {
+                loadProgress { progress ->
+                    currentProgress = progress
+                }
+                loading = false // Reset loading when the coroutine finishes
+            }
+        }, enabled = !loading) {
+            Text("Start loading")
+        }
+
+        if (loading) {
+            LinearProgressIndicator(
+                progress = currentProgress,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+/** Iterate the progress value */
+suspend fun loadProgress(updateProgress: (Float) -> Unit) {
+    for (i in 1..100) {
+        updateProgress(i.toFloat() / 100)
+        delay(100)
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AndroidWebviewPreview() {
+    WebviewLifecycleTheme {
+//        AndroidTestWebView()
+    }
+}
+
+
